7895ab610be0ec6e260eb8e9055053ad585b8d3d
Jean-Marc <mjean_marc@hotmail.com>
2025-10-28 15:32:07 +0100

    Retrieve id with useParams

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { getVans } from "../../api";
+import { Link } from "react-router-dom";
 
 export default function HostVans() {
     const [vans, setVans] = React.useState([]);
@@ -12,12 +13,14 @@ export default function HostVans() {
 
     const vanElements = vans.map(van => (
         <div key={van.id} className="van-tile">
-            <img src={van.imageUrl} />
-            <div className="van-info">
-                <h3>{van.name}</h3>
-                <p>${van.price}<span>/day</span></p>
-            </div>
-            <i className={`van-type ${van.type} selected`}>{van.name}</i>
+            <Link to={`/vans/${van.id}`}>
+                <img src={van.imageUrl} />
+                <div className="van-info">
+                    <h3>{van.name}</h3>
+                    <p>${van.price}<span>/day</span></p>
+                </div>
+                <i className={`van-type ${van.type} selected`}>{van.name}</i>
+            </Link>
         </div>
     ));
 
---
+++ steps.md
@@ -0,0 +1,7 @@
+# Van Life
+
+Steps
+
+## Add links to van detail page
+
+Wrap the contents of the "van-tile" div in a Link that sends the user to `/vans/${van-id-here}`.
