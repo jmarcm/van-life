4885587b2b5277371732ef5618f8dc64665efa1e
Jean-Marc <mjean_marc@hotmail.com>
2025-10-28 11:54:15 +0100

    Start main menu

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,24 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
+import Vans from "./pages/Vans/Vans"
 import "./App.css"
 
 export default function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
-        <nav>
-          <Link to="/about">About</Link>
-        </nav>
-      </header>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
+        <header>
+            <Link className="site-logo" to="/">#VanLife</Link>
+            <nav>
+            <Link to="/about">About</Link>
+            <Link to="/vans">Vans</Link>
+            </nav>
+        </header>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/vans" element={<Vans />}/>
+        </Routes>
     </BrowserRouter>
   )
 }
---
+++ src/pages/HostVans.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+export default function HostVans() {
+    return <h1>HostVans Page</h1>;
+}
