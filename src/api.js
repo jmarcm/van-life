906545ce92d0b7e3864a156e290e26424759f48d
Jean-Marc <mjean_marc@hotmail.com>
2025-11-10 10:02:47 +0100

    Cloud Firestore Code setup

--- src/api.js
+++ src/api.js
@@ -1,3 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+import { getFirestore } from "firebase/firestore/lite";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAZdL9nW0gHTEnOKCMApQVO4FbOEqrgvDQ",
+    authDomain: "vanlife-2ba5f.firebaseapp.com",
+    projectId: "vanlife-2ba5f",
+    storageBucket: "vanlife-2ba5f.firebasestorage.app",
+    messagingSenderId: "188325408045",
+    appId: "1:188325408045:web:880c4628449de37ba4f7a4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 export async function getVans() {
     const res = await fetch("/api/vans");
 
